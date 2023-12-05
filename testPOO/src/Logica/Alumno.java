ef09b710481eff02ffb5747585a926a56f58207c
Moortin35 <moortindelgado@gmail.com>
2023-12-05 17:28:02 -0300

    Se agrego el constructor de la clase alumno

--- testPOO/src/Logica/Alumno.java
+++ testPOO/src/Logica/Alumno.java
@@ -4,6 +4,14 @@ public class Alumno {
     int id;
     String nombre;
     String apellido;
+
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
     
     public void mostrarNombre(){
         System.out.println("Hola soy un alumno, y tengo nombre");
--- testPOO/src/Logica/TestPOO.java
+++ testPOO/src/Logica/TestPOO.java
@@ -3,7 +3,7 @@ package Logica;
 public class TestPOO {
 
     public static void main(String[] args) {
-        
+        Alumno alumno1 = new Alumno(01, "John", "Smith");
     }
     
 }
